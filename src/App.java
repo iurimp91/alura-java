3333fc27aa46714bf7296604fae248fee6b32dc4
Iuri Magnago <iurimp91@gmail.com>
2022-07-21 18:52:27 -0300

    create project and get movies data

---
+++ src/App.java
@@ -0,0 +1,21 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+  public static void main(String[] args) throws Exception {
+    String url = "https://alura-filmes.herokuapp.com/conteudos";
+    URI address = URI.create(url);
+    HttpClient client = HttpClient.newHttpClient();
+    var request = HttpRequest.newBuilder(address).GET().build();
+    HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+    String body = response.body();
+    System.out.println(body);
+
+    List<Map<String, String>> moviesList = body;
+  }
+}
